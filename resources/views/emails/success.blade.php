d3f2cb30240c6909c55b4e19c871f5a7386b51f2
Nick Ashford <nick1973_5@btinternet.com>
2016-12-21 14:27:35 +0000

    emails on passed

--- resources/views/emails/success.blade.php
+++ resources/views/emails/success.blade.php
@@ -1,6 +1,6 @@
 <p>Hi from Project Planner,</p>
 <h3>Requirements</h3>
-<p>{{ $requirements->requirements }}</p>
-<h3>Process</h3>
-<p>{{ $requirements->rocess }}</p>
-<p>Has been completed</p>
+{{--<p>{{ $requirements->requirements }}</p>--}}
+{{--<h3>Process</h3>--}}
+{{--<p>{{ $requirements->rocess }}</p>--}}
+{{--<p>Has been completed</p>--}}
