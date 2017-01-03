cf5bfd27c91d41b0930f1ee47cb147fac4a3bb2e
Nick Ashford <nick1973_5@btinternet.com>
2017-01-03 12:44:22 +0000

    Development radio buttons

--- app/Requirements.php
+++ app/Requirements.php
@@ -7,7 +7,9 @@ use Illuminate\Database\Eloquent\Model;
 class Requirements extends Model
 {
     protected $fillable = [
-        'project_id', 'name', 'requirements', 'process', 'url', 'tested_by', 'passed', 'bugs', 'aesthetic', 'wish_list', 'development'
+        'project_id', 'name', 'requirements', 'process',
+        'url', 'tested_by', 'passed', 'bugs', 'aesthetic', 'wish_list',
+        'development'
     ];
 
 }
